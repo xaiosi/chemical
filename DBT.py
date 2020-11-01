2b04acb75e671ed0851e84e49007b14d2e7b7f6b
哓肆 <58328985+xaiosi@users.noreply.github.com>
2020-11-01 14:38:50 +0800

    Update DBT.py

--- DBT.py
+++ DBT.py
@@ -74,12 +74,3 @@ def Flow(num):
 
 
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("迎张奇爸爸！！\n")
-    # print_hi('Zhangqi')
-    # Tran(1000,'water')
-    Flow(170)
-    # material_splite(5,2)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
