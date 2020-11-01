8d5c03c8b982f1a16447319f6ddaa93800e5fce6
xaiosi <zhangqiyouxiang2019@outlook.com>
2020-11-01 14:37:21 +0800

    add Flow()

--- DBT.py
+++ DBT.py
@@ -35,8 +35,51 @@ def material_splite(num, wher):
             print(contain[j])
         elif int(temp3) == res[1]:
             print(contain[j])
+
+def Flow(num):
+    material = np.array([0.0005,0.0121,0.0287,0.5582,0.3372,0.0349,0.0203,0.0081])
+    contain = ['a1','a2','a3','a4','a5','a6','a7','a8']
+    QnF_list = []
+    QnF = num
+    QnD = 0
+    QnW = 0
+    for k in range(8):
+        m = material[k] * QnF
+        QnF_list.append(m)
+    for i in range(3):
+        temp = QnF_list[i]
+        temp1 = QnF_list[7-i]
+        QnD = QnD + temp
+        QnW = QnW + temp1
+    QnD = QnD + QnF_list[3]*0.83 + QnF_list[4]*0.27
+    QnW = QnW + QnF_list[3]*0.17 + QnF_list[4]*0.73
+    for j in range(8):
+        print('QnF(a %d ):%f' % (j+1,QnF_list[j]))
+    print('*'*30)
+    print('QnD:%f' % (QnD))
+    print('Qnw:%f' % (QnW))
+    print('-'*30)
+    for i in range(8):
+        if i <= 2:
+            print('X(a%d-D):%f' % (i+1, QnF_list[i] / QnD))
+        elif i == 3:
+            print('X(a4-D):%f' % (QnF_list[i]*0.83 / QnD))
+            print('X(a4-W):%f' % (QnF_list[i] * 0.17 / QnW))
+        elif i == 4:
+            print('X(a5-D):%f' % (QnF_list[i]*0.27 / QnD))
+            print('X(a5-W):%f' % (QnF_list[i] * 0.73 / QnW))
+        else:
+            print('X(a%d-W):%f' % (i+1,QnF_list[i] / QnW))
+
+
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Zhangqi')
-    material_splite(5,2)
+    print("迎张奇爸爸！！\n")
+    # print_hi('Zhangqi')
+    # Tran(1000,'water')
+    Flow(170)
+    # material_splite(5,2)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ mian.py
@@ -0,0 +1,12 @@
+import DBT
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("迎张奇爸爸！！\n")
+    # print_hi('Zhangqi')
+    # Tran(1000,'water')
+    DBT.Flow(170)
+    # material_splite(5,2)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
